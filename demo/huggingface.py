d863ac0a705ca98af93227864f37768aeac3198c
FrankieShih <61614341+FrankieShih@users.noreply.github.com>
2023-08-01 12:34:43 -0400

    Create huggingface.py
    A jupyter demo of Llama-2-chat-7b via huggingface

---
+++ demo/huggingface.py
@@ -0,0 +1,36 @@
+!pip install transformers
+!pip install xformers
+
+import transformers
+from transformers import AutoTokenizer
+import torch
+import xformers
+
+model = "tiiuae/falcon-7b-instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto",
+)
+
+with open('test', 'r') as file:
+    text = file.read()
+
+prompt = f"{text}"
+print(prompt)
+
+sequences = pipeline(
+    prompt,
+    max_length=1000,
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
